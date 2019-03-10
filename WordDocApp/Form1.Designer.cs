6b4575038f65ac304efdab21d3aff6d17f8a86de
AndreiR <AndreiR@DESKTOP-G22DBJE>
2019-03-10 21:59:40 +0200

    Add project files.

---
+++ WordDocApp/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WordDocApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.generateDocButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // generateDocButton
+            // 
+            this.generateDocButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.generateDocButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateDocButton.Image = ((System.Drawing.Image)(resources.GetObject("generateDocButton.Image")));
+            this.generateDocButton.Location = new System.Drawing.Point(196, 363);
+            this.generateDocButton.Name = "generateDocButton";
+            this.generateDocButton.Size = new System.Drawing.Size(212, 67);
+            this.generateDocButton.TabIndex = 0;
+            this.generateDocButton.Text = "Generate Doc.";
+            this.generateDocButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.generateDocButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.generateDocButton.UseMnemonic = false;
+            this.generateDocButton.UseVisualStyleBackColor = false;
+            this.generateDocButton.Click += new System.EventHandler(this.generateDocButton_Click);
+            this.generateDocButton.MouseLeave += new System.EventHandler(this.generateDocButton_MouseLeave);
+            this.generateDocButton.MouseHover += new System.EventHandler(this.generateDocButton_MouseHover);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(190, 186);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(224, 22);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(190, 139);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(224, 22);
+            this.firstNameTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "First Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 496);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.generateDocButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WordApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generateDocButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WordDocApp/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Drawing;
+using MetroFramework.Forms;
+using Microsoft.Office.Interop.Word;
+using System.Reflection;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WordDocApp
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Metoda se uita in fisierul nostru word si inlocuieste textul introdus dupa cerintele noastre
+        /// Este metoda Replace din documentul word (buton, colt dreapta sus)
+        /// </summary>
+        private void FindAndReplaceWords(Microsoft.Office.Interop.Word.Application wordApp, object toFindText, object replaceWithText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildcards = false;
+            object matchSoundsLike = false;
+            object MatchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacrits = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object readOnly = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+
+            //documentatie: https://docs.microsoft.com/en-us/dotnet/api/microsoft.office.interop.word.find.execute?view=word-pia
+            wordApp.Selection.Find.Execute(ref toFindText, ref matchCase, ref matchWholeWord,
+                ref matchWildcards, ref matchSoundsLike, ref MatchAllWordForms, ref forward, ref wrap, ref format,
+             ref replaceWithText, ref replace, ref matchKashida, ref matchDiacrits, ref matchAlefHamza, ref matchControl);
+        }
+
+        private void GenerateWordDocument(object fileName, object saveAsFileName)
+        {
+            Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+
+            //2. ne generam un obiect Missing -> ii spunem metodei de jos ca nu vrem sa folosim acel parametru
+            object missing = Missing.Value;
+            //3. acesta este documentul ce va fi generat si in care ne vom salva informatia
+            Document wordFile = new Microsoft.Office.Interop.Word.Document();
+
+            //4. verificam daca exista acest fisier
+            bool isValidFile = File.Exists((string)fileName);
+            if (isValidFile)
+            {
+                //5. readonliy e de fapt true, doar ca la deschidere trebuie sa fie false pentru a putea modifica documentul
+                object readOnly = false;
+                //6 isVisibile = false, pentru ca nu ne interesaza bucataria interna si ce se intampla acolo
+                object isVisible = false;
+                //7. wordApp.Visibile = false pentru ca nu vrem sa ne apara documentul la generare pe ecran, daca vrem asta punem true.
+                wordApp.Visible = false;
+
+                //8. aici deschidem fisierul Word, dupa care ii setam <fileName>, trecem parametri missing 
+                // pentru ca nu sunt necesari si dupa care setam optiunea de readOnly pentru a putea modifica documentul
+                wordFile = wordApp.Documents.Open(ref fileName, ref missing, ref readOnly,
+                    ref missing, ref missing, ref missing, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+
+                wordFile.Activate();
+
+                //9. aici facem modificarea cuvintelor
+                FindAndReplaceWords(wordApp, "<name>", nameTextBox.Text);
+                FindAndReplaceWords(wordApp, "<firstName>", firstNameTextBox.Text);
+                FindAndReplaceWords(wordApp, "<date>", DateTime.Now.ToString("dd.MM.yyyy"));
+            }
+            else
+            {
+                //daca nu functioneaza apare mesajul
+                System.Windows.Forms.MessageBox.Show("File not found!", "File not found!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            //10. Salvam documentul, saveAs va fi calea catre documentul nostru
+            wordFile.SaveAs2( ref saveAsFileName, ref missing, ref missing, ref missing, ref missing,
+                ref missing, ref missing, ref missing, ref missing, ref missing, ref missing,
+                ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+            //11. inchidem fisierul Word
+            wordFile.Close();
+            //12. inchidem aplicatia Word
+            wordApp.Quit();
+
+            //mesaj de feedback- ca a functionat
+            MessageBox.Show("File done!", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void generateDocButton_MouseHover(object sender, EventArgs e)
+        {
+            generateDocButton.BackColor = Color.Teal;
+        }
+
+        private void generateDocButton_MouseLeave(object sender, EventArgs e)
+        {
+            generateDocButton.BackColor = Color.White;
+        }
+
+        private void generateDocButton_Click(object sender, EventArgs e)
+        {
+            object pathToFileName = @"C:\Users\AndreiR\Documents\Visual Studio 2015\Projects\WordGenerator\WordApp\WordDocApp\Certificate_template.docx";
+            object pathToSaveAs = @"C:\Users\AndreiR\Documents\Visual Studio 2015\Projects\WordGenerator\WordApp\WordDocApp\Certificate_template1.docx";
+            GenerateWordDocument(pathToFileName, pathToSaveAs);
+        }
+    }
+}
